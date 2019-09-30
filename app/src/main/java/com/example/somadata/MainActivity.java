3fa318d2f0ed60ce4af705b33161d236cffdda82
Joseph Ridge <josephridge18@gmail.com>
2019-09-30 10:46:56 +0300

    Test

--- app/src/main/java/com/example/somadata/MainActivity.java
+++ app/src/main/java/com/example/somadata/MainActivity.java
@@ -7,8 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Color;
 import android.os.Bundle;
+
 import android.util.Log;
-import android.widget.EditText;
+
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.LineChart;
@@ -23,115 +24,106 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Objects;
+    public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
-
-    LineChart lineChart ;
-    TextView result,tempresult, humidityresult;
-    ArrayList<Entry>yDATA,ydata;
-    TextView HUMIDITY;///NEW!!
-
-    private static final String LOG_TAG;
+        LineChart lineChart ;
+        TextView result,tempresult, trix,logresult,humidity, soil_moisture, temperature,optimus;
+        ArrayList<Entry>yDATA,xAxis,ydata,hdata,tdata;
+       private List<PayloadOutputs> humidityValues;
 
 
-    static {
+        private static final String LOG_TAG;
+        static {
             LOG_TAG = MainActivity.class.getSimpleName();
         }
 
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference humid = database.getReference("Member");///il travaille
+
+        DatabaseReference humido =  database.getReference("payload/-LplVF-Tt8knbu4T09Kg/metadata/data_rate");////il travaille
 
         @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        final FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-        DatabaseReference humido =  database.getReference().child("TechShamba/-LpYVdWqieWjr2XXr-rQ/hardware_serial");////RETURNS INTEGER VALUE ,IT WORKS IN RESPONSE
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            humidity = findViewById(R.id.Humidityrecord);
+            temperature = findViewById(R.id.tempraturerecord);
+            soil_moisture =  findViewById(R.id.inputedoutput);
 
-        DatabaseReference tempgraph = database.getReference("TechShamba");
+           // getData();
+            //plot_graph();
+            lineChart = (LineChart) findViewById(R.id.lineChart);
+
+           /* humid.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
 
 
-        DatabaseReference humid = database.getReference("Member");///ALLOWS PLOTTING OF THE AGE GRAPH
-        DatabaseReference mygrapgh = database.getReference("TechShamba/payload");
-        lineChart = (LineChart) findViewById(R.id.lineChart);
-        humidityresult = (TextView) findViewById(R.id.Humidityrecord);
-
-
-   humid.addValueEventListener(new ValueEventListener() { //plots a graph of all ages collected
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                yDATA = new ArrayList<>();
-                float i = 0;
-                for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                    i = i + 1;
-                    String SV = Objects.requireNonNull(ds.child("age").getValue()).toString();//member/age
-                    float SensorValue = Float.parseFloat(SV);//parser
-                    yDATA.add(new Entry(i, SensorValue));
+                    Log.d(LOG_TAG,"Value is :  "+ dataSnapshot);
                 }
-                final LineDataSet lineDataSet1 = new LineDataSet( yDATA, "age" );
-                LineData data = new LineData(lineDataSet1);
-                lineChart.setData(data);
-                lineChart.notifyDataSetChanged();
-                lineChart.invalidate();
 
-                lineDataSet1.setDrawCircles(false);
-                lineDataSet1.setColors(Color.RED);
- //ss
-            }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e(LOG_TAG,"Errr!!! ");
-            }
-        });
-   /*
-   mygrapgh.addValueEventListener(new ValueEventListener() {
-       @Override
-       public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-           ydata = new ArrayList<>();
-           float i = 0;
-           for (DataSnapshot ds: dataSnapshot.getChildren()){
-               i=i++;
-               String value = Objects.requireNonNull(ds.child("temperature").getValue()).toString();
-               float Sensorvue= Float.parseFloat(value);
-               yDATA.add(new Entry(i, Sensorvue));
-           }
-           final LineDataSet lineDataSet2 = new LineDataSet(ydata,"Temp.");
-           LineData dta = new LineData(lineDataSet2);
-           lineChart.setData(dta);
-           lineChart.notifyDataSetChanged();
-           lineChart.invalidate();
+                }
+            });*/
 
-           lineDataSet2.setColor(Color.GREEN);
-           lineDataSet2.setDrawCircles(false);
-       }
+                Log.d(LOG_TAG,"Before attaching the listener ");
+            DatabaseReference classy = database.getReference( ).child("TechShamba");
+           classy.addValueEventListener(new ValueEventListener() {
+               @Override
+               public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                   float i=0;
+                   for (DataSnapshot ds:dataSnapshot.getChildren()) {
+                       i = i+1;
+                       yDATA = new ArrayList<>(); //Initialize the graph
+                       Object x = ds.child("payload_fields/humidity").getValue();  //get value
+                       Object y = ds.child("payload_fields/temperature").getValue();//get value
+                       Object z = ds.child("payload_fields/soilMoisture").getValue();//get value
+                       String hm = String.valueOf(y);
+                       Float hum = Float.parseFloat(hm);
+                       yDATA.add(new Entry(i ,hum));
 
-       @Override
-       public void onCancelled(@NonNull DatabaseError databaseError) {
+                       Log.d(LOG_TAG, "Humidity: " + x);//confirm its success by logging it
+                       Log.d(LOG_TAG, "Temperature: " + y);//confirm its success by logging it
+                       Log.d(LOG_TAG, "SoilMositure: " + z + "\n");//confirm its success by logging it
 
-       }
-   });
-*/
+                      //String hm = String.valueOf(x);//converting to String from Long
+                       Log.d(LOG_TAG, "HMIS PRINTED :  " + hm); //printing it out to log cat
+                     //  Float hum = Float.parseFloat(hm); //parsing it into float
+                       Log.d(LOG_TAG, "HUMIDITY RESULT" + hum); //printing it out to the logcat
 
-   result = (TextView)findViewById(R.id.TimeOfRecord);
+                       String SV = "100"; //TESTING PURPOSES
+                       yDATA.add(new Entry(i, hum)); //ADD VALUES TO GRAPH
 
-        humido.addValueEventListener(new ValueEventListener() {//plotted well
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String Val = dataSnapshot.getValue(String.class);
-                result.setText(String.valueOf(Val));
-                Log.d(LOG_TAG ,"Value is: "+Val);
+                      }
+                           final LineDataSet lineDataSet1 = new LineDataSet(yDATA, "Temp.");
+                           LineData data = new LineData(lineDataSet1);
+                           lineChart.setData(data);
+                           lineChart.notifyDataSetChanged();
+                           lineChart.invalidate();
+
+                           lineDataSet1.setDrawCircles(false);
+                           lineDataSet1.setColors(Color.GRAY);}
 
 
-            }
+               @Override
+               public void onCancelled(@NonNull DatabaseError databaseError) {
+                   Log.w(LOG_TAG,"loaddata failed",databaseError.toException()); //incase of error
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.e(LOG_TAG ,"ERRR!! ");
 
-            }
-        });
-    }}
+
+               }
+           });
+        }
+    }
+
+
